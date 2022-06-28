498f1479b691b02e7a2d51d22f87ab73f75989cb
Anna1234-dot <103524983+Anna1234-dot@users.noreply.github.com>
2022-06-28 21:27:43 +1000

    Add files via upload

---
+++ src/components/Results.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Best time results by levels</h1>
+    <div>
+        <h2>Easy: {{ time1 }}</h2>
+        <h2>Medium:  {{ time2 }}</h2>
+        <h2>Hard: {{ time3 }} </h2>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent, PropType } from 'vue';
+import { Playing, time1, time2, time3 } from '../game'
+import Game from './Game.vue'
+
+export default defineComponent({
+    name: 'Results',
+    setup(){
+        return{
+            time1,
+            time2,
+            time3
+        }
+    }
+});
+
+</script>
